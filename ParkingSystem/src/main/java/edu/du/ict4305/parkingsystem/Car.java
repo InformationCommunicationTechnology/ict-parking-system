3a47638eab991187d18ea9d6565a7f4597361415
Luther <luther.chikumba@guildeducation.com>
2022-10-04 08:48:23 -0600

    Update class car with fields, getters and setter methods

--- ParkingSystem/src/main/java/edu/du/ict4305/parkingsystem/Car.java
+++ ParkingSystem/src/main/java/edu/du/ict4305/parkingsystem/Car.java
@@ -7,6 +7,8 @@
  */
 package edu.du.ict4305.parkingsystem;
 
+import java.time.LocalDate;
+
 /**
  * 
  * @Date: 9/29/2022
@@ -14,5 +16,51 @@ package edu.du.ict4305.parkingsystem;
  *
  */
 public class Car {
+    private String permit;
+    private LocalDate permitExpiration;
+    private String license;
+    private CarType type;
 
+    public  Car(){
+
+    }
+
+    public String getPermit() {
+        return permit;
+    }
+
+    public void setPermit(String permit) {
+        this.permit = permit;
+    }
+
+    public LocalDate getPermitExpiration() {
+        return permitExpiration;
+    }
+
+    public void setPermitExpiration(LocalDate permitExpiration) {
+        this.permitExpiration = permitExpiration;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public void setType(CarType type) {
+        this.type = type;
+    }
+
+    public String owner(Customer id){
+        return "";
+    }
+    public String toString(){
+        return "";
+    }
 }
