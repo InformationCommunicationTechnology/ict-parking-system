6b9ecc9f64acf543c9ddd92c85ff1e27d0afbb6f
Luther <luther.chikumba@guildeducation.com>
2022-10-05 14:54:57 -0600

    Add test to all of the classes.

---
+++ ParkingSystem/src/test/java/edu/du/ict4305/parkingsystem/CustomerTest.java
@@ -0,0 +1,46 @@
+package edu.du.ict4305.parkingsystem; /**
+ * @Course: ICT 4305
+ * @Project: ParkingSystem
+ * @Instructor: Dr. Sherri Maciosek
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @Date: 10/5/22
+ * @author lutherchikumba
+ *
+ */
+public class CustomerTest {
+
+      public CustomerTest() {
+
+      }
+
+      @Test
+      public void getCustomerId() {
+      }
+
+      @Test
+      public void getName() {
+      }
+
+      @Test
+      public void getAddress() {
+      }
+
+      @Test
+      public void getPhoneNumber() {
+      }
+
+      @Test
+      public void register() {
+      }
+
+      @Test
+      public void testToString() {
+      }
+}
