a51148f63170e2f07c29f5a3e5fb9400f5d44bff
Luther <luther.chikumba@guildeducation.com>
2022-10-05 14:39:08 -0600

    List the car types

--- ParkingSystem/src/main/java/edu/du/ict4305/parkingsystem/CarType.java
+++ ParkingSystem/src/main/java/edu/du/ict4305/parkingsystem/CarType.java
@@ -12,14 +12,6 @@ package edu.du.ict4305.parkingsystem;
  * @Date: 9 /29/2022
  */
 public enum CarType {
-      ;
-
-      private String COMPACT;
-      private String SUV;
-
-      private CarType(String compact, String suv) {
-            this.COMPACT = compact;
-            this.SUV = suv;
-      }
-
+      COMPACT,
+      SUV;
 }
