a7f3e4f77ed14f559daf8a0eac58ccb3b27f5d00
Luther <luther.chikumba@guildeducation.com>
2022-10-06 17:00:26 -0600

    Test CustomerTest and ParkingLotTest classes

--- ParkingSystem/src/test/java/edu/du/ict4305/parkingsystem/CustomerTest.java
+++ ParkingSystem/src/test/java/edu/du/ict4305/parkingsystem/CustomerTest.java
@@ -6,57 +6,72 @@ package edu.du.ict4305.parkingsystem; /**
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
- * @Date: 10/5/22
  * @author lutherchikumba
- *
+ * @Date: 10/5/22
  */
 public class CustomerTest {
+      Customer customer;
+      Address address;
 
       public CustomerTest() {
-
+            address = new Address("4581 S Valdai Way.", "Aurora", "CO", "80015");
+            customer = new Customer("luther", "303422425", address, "##########");
       }
 
       @Test
       public void getCustomerId() {
+            assertEquals("303422425", customer.getCustomerId());
       }
 
       @Test
       public void setCustomerId() {
+            customer.setCustomerId("303422425");
+            assertEquals("303422425", customer.getCustomerId());
       }
 
       @Test
       public void getName() {
+            assertEquals("luther", customer.getName());
       }
 
       @Test
       public void setName() {
+            customer.setName("luther");
+            assertEquals("luther", customer.getName());
       }
 
       @Test
       public void getAddress() {
+            assertEquals(address, customer.getAddress());
       }
 
       @Test
       public void setAddress() {
+            customer.setAddress(address);
+            assertEquals(address, customer.getAddress());
       }
 
       @Test
       public void getPhoneNumber() {
+            assertEquals("##########", customer.getPhoneNumber());
       }
 
       @Test
       public void setPhoneNumber() {
+            customer.setPhoneNumber("##########");
+            assertEquals("##########", customer.getPhoneNumber());
       }
 
       @Test
       public void register() {
+            LocalDate date = LocalDate.now();
+            CarType type = CarType.SUV;
+            assertEquals("YYY:350", customer.register("YYY:350", type).getLicense());
       }
 
-      @Test
-      public void testToString() {
-      }
 }
